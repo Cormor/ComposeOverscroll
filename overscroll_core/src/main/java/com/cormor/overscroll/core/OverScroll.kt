f2fc71ba302ff1a4443cf4efbda91a07d7f6cadb
Cormor <cangtiansuo@gmail.com>
2023-08-18 18:47:41 +0800

    优化代码细节

--- overscroll_core/src/main/java/com/cormor/overscroll/core/OverScroll.kt
+++ overscroll_core/src/main/java/com/cormor/overscroll/core/OverScroll.kt
@@ -153,9 +153,9 @@ fun Modifier.overScrollOutOfBound(
                 return if (sign(offset) != sign(offsetAtLast)) {
                     offset = 0f
                     if (isVertical) {
-                        Offset(x = available.x - realAvailable.x, y = available.y - realAvailable.y + offsetAtLast)
+                        Offset(x = available.x - realAvailable.x, y = available.y - realAvailable.y + offset + realOffset)
                     } else {
-                        Offset(x = available.x - realAvailable.x + offsetAtLast, y = available.y - realAvailable.y)
+                        Offset(x = available.x - realAvailable.x + +offset + realOffset, y = available.y - realAvailable.y)
                     }
                 } else {
                     offset = offsetAtLast
